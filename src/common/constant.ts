3292368e173491ef1efb7bfb4dc962a35fcd6eb9
Jewel <ylingzhu2022@gmail.com>
2024-12-30 11:30:26 +0800

    fix: Unable to git clone flutter_normal_demo

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -1,3 +1,4 @@
 export const FLUTTER_DEMO_DIR = 'flutter_normal_demo';
 export const REPO_URL = 'https://github.com/Jewel105/flutter_normal_demo.git';
+export const REPO_URL_BACKUP = 'https://gitee.com/Abner105/flutter_normal_demo.git';
 export const LAST_PULL_TIME_KEY = 'last_pull_time';
--- src/common/tools.ts
+++ src/common/tools.ts
@@ -2,7 +2,7 @@ import { execSync } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 import * as vscode from 'vscode';
-import { LAST_PULL_TIME_KEY } from './constant';
+import { LAST_PULL_TIME_KEY, REPO_URL_BACKUP } from './constant';
 
 
 /**
@@ -54,36 +54,62 @@ function copyFile(sourcePath: string, destinationPath: string) {
  * @param repoUrl 远程仓库地址.
  * @param demoDir 本地存储文件夹名称.
  */
-export function getGithub(repoUrl: string, demoDir: string, context: vscode.ExtensionContext): void {
+export async function getGithub(repoUrl: string, demoDir: string, context: vscode.ExtensionContext): Promise<void> {
     const tempDir = path.resolve(__dirname, demoDir);
-    if (!fs.existsSync(tempDir)) {
-        console.log('开始克隆项目...');
-        // 克隆项目
-        fs.mkdirSync(tempDir, { recursive: true });
-        execSync(`git clone ${repoUrl} ${tempDir}`, { stdio: 'inherit' });
-        const timestamp = Date.now();
-        context.globalState.update(LAST_PULL_TIME_KEY, timestamp);
-        console.log('克隆完成...', timestamp);
-    } else {
-        // 检查是否需要更新
-        const lasttime: number = context.globalState.get(LAST_PULL_TIME_KEY,) ?? 0;
-        const now = Date.now();
-        if (lasttime > now - 1000 * 60 * 60 * 24 * 3) {
-            console.log('3天内拉取过代码，无需更新...', lasttime);
-            return;
-        }
-        console.log('检查更新...', lasttime);
-        // 获取本地项目的最新版本号
-        const localHash = execSync(`git -C ${tempDir} rev-parse HEAD`).toString().trim();
-        // 获取远程仓库的最新版本号
-        const remoteHash = execSync(`git ls-remote ${repoUrl} HEAD`).toString().split('\t')[0].trim();
-        if (localHash !== remoteHash) {
-            console.log('版本号不同，开始拉取更新...');
-            execSync(`git -C ${tempDir} pull`, { stdio: 'inherit' });
+    try {
+        const pubspacePath = path.join(tempDir, 'pubspec.yaml');
+        if (!fs.existsSync(pubspacePath)) {
+            console.log('开始克隆项目...');
+            // 克隆项目
+            fs.mkdirSync(tempDir, { recursive: true });
+            // 弹出加载框
+            await vscode.window.withProgress(
+                {
+                    location: vscode.ProgressLocation.Notification,
+                    title: 'Flutter Project Forge: git clone flutter_normal_demo ...',
+                },
+                async (_, __) => {
+                    execSync(`git clone ${repoUrl} ${tempDir}`, { stdio: 'inherit' });
+                    return 'Flutter Project Forge: git clone completed!';
+                }
+            ).then((result) => {
+                vscode.window.showInformationMessage(result); // 弹出任务完成后的提示
+            });
+            const timestamp = Date.now();
+            context.globalState.update(LAST_PULL_TIME_KEY, timestamp);
+            console.log('克隆完成...', timestamp);
         } else {
-            console.log('版本号相同，无需更新。');
+            // 检查是否需要更新
+            const lasttime: number = context.globalState.get(LAST_PULL_TIME_KEY,) ?? 0;
+            const now = Date.now();
+            if (lasttime > now - 1000 * 60 * 60 * 24 * 3) {
+                console.log('3天内拉取过代码，无需更新...', lasttime);
+                return;
+            }
+            console.log('检查更新...', lasttime);
+            // 获取本地项目的最新版本号
+            const localHash = execSync(`git -C ${tempDir} rev-parse HEAD`).toString().trim();
+            // 获取远程仓库的最新版本号
+            const remoteHash = execSync(`git ls-remote ${repoUrl} HEAD`).toString().split('\t')[0].trim();
+            if (localHash !== remoteHash) {
+                console.log('版本号不同，开始拉取更新...');
+                execSync(`git -C ${tempDir} pull`, { stdio: 'inherit' });
+            } else {
+                console.log('版本号相同，无需更新。');
+            }
+            context.globalState.update(LAST_PULL_TIME_KEY, now);
         }
-        context.globalState.update(LAST_PULL_TIME_KEY, now);
+    } catch (error) {
+        console.log('拉取项目失败...');
+        console.log(error);
+        fs.rmSync(tempDir, { recursive: true, force: true });
+        if (REPO_URL_BACKUP === repoUrl) {
+            vscode.window.showErrorMessage("Flutter Project Forge: NETWORK ERROR");
+        } else {
+            // 从备份仓库再拉一次
+            console.log('从备份仓库再拉一次...');
+            getGithub(REPO_URL_BACKUP, demoDir, context);
+        }
     }
 }
 /**
@@ -139,10 +165,10 @@ export function writeContentToFile(content: string, destinationPath: string, fil
  * @param targetFilePath 文件或文件夹路径
  * @returns 文件夹的路径
  */
-export function getCurrentDir(targetFilePath: string):string {
-  var stats = fs.statSync(targetFilePath);
-  if (stats.isFile()) {
-    targetFilePath = path.dirname(targetFilePath);
-  }
-  return targetFilePath;
+export function getCurrentDir(targetFilePath: string): string {
+    var stats = fs.statSync(targetFilePath);
+    if (stats.isFile()) {
+        targetFilePath = path.dirname(targetFilePath);
+    }
+    return targetFilePath;
 }
